94ff2a939e961d1cfdff95c77e23ebb2b2495607
avikd1001 <73572571+avikd1001@users.noreply.github.com>
2022-07-28 10:51:44 +0530

    Uploaded JUnit project files

---
+++ FST_JUnit/src/test/java/examples/CalculatorTest.java
@@ -0,0 +1,30 @@
+package examples;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+// The Test class should start or end with "Test"
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        calculator = new Calculator();
+    }
+
+    @Test
+    @DisplayName("Simple multiplication should work")
+    public void testMultiply() {
+        assertEquals(20, calculator.multiply(4, 5));
+    }
+
+    @Test
+    @DisplayName("Simple addition should work")
+    public void testAdd() {
+        assertEquals(16, calculator.add(11, 5));
+    }
+}
